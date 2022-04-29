5315dd7eb89735821ebd780c4cd735c5121e55ff
Vincent Lee <vinnyclee@gmail.com>
2022-04-29 16:44:41 -0700

    added Food_Order class

---
+++ Food_Order.java
@@ -0,0 +1,21 @@
+public class Food_Order {
+	private Food food;
+	private int quantity;
+	
+	public Food_Order(Food f,int q) {
+		food=f;
+		quantity=q;
+	}
+	public Food getFood() {
+		return food;
+	}
+	public void setFood(Date f) {
+		food=f;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int q) {
+		quantity=q;
+	}
+}
