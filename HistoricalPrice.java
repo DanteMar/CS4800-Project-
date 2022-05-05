2d5d7d07450f4f075c542df86104b872d6f37b46
dante <79384307+DanteMar@users.noreply.github.com>
2022-05-04 19:37:38 -0700

    added create method and variables

--- HistoricalPrice.java
+++ HistoricalPrice.java
@@ -1,4 +1,38 @@
+import java.sql.Connection;
 
 public class HistoricalPrice {
-
+	private int priceID;
+	private double price;
+	private Date d;
+	
+	public void newHistoricalPrice(double price, Date d)
+	{
+		try
+		{
+			Connection connection = LoginDataAccess.verifyCredentials();
+			// create the java statement
+			
+			//Generate appropriate query
+			String query = "INSERT INTO `historicalPrice` (`price`, `d`)";
+			
+			// execute the query
+			connection.createStatement().executeUpdate(query);
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	public int getID()
+	{
+		return priceID;
+	}
+	public Date getDate()
+	{
+		return d;
+	}
+	public double getPrice()
+	{
+		return price;
+	}
 }
