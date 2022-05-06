7d38b30a1c9f86d7bf39c18d9153fe68eb7a0f80
dante <79384307+DanteMar@users.noreply.github.com>
2022-05-05 19:43:52 -0700

    added queries

--- Order.java
+++ Order.java
@@ -1,3 +1,5 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -15,6 +17,58 @@ public class Order {
 		listOfProducts= new ArrayList<Food_Order>();
 		
 		
+	}
+	public void newOrder(Date date, Time time, Customer customer, String status)
+	{
+		try
+		{
+			Connection connection = LoginDataAccess.verifyCredentials();
+			// create the java statement
+			
+			//Generate appropriate query
+			String query = "INSERT INTO `order` (`date`, `time`, `customer`, `status` )";
+			
+			// execute the query
+			connection.createStatement().executeUpdate(query);
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	public void updateOrder(int orderID, Date date, Time time, Customer customer, String status)
+	{
+		try
+		{
+			Connection connection = LoginDataAccess.verifyCredentials();
+			// create the java statement
+			
+			//Generate appropriate query
+			// all database values will change once database is setup
+			connection.createStatement().executeUpdate("UPDATE `order` SET `date` = '" + date + "' WHERE `itemID` = " + orderID);	
+			connection.createStatement().executeUpdate("UPDATE `order` SET `time` = '" + time + "' WHERE `itemID` = " + orderID);
+			connection.createStatement().executeUpdate("UPDATE `order` SET `customer` = '" + customer + "' WHERE `itemID` = " + orderID);
+			connection.createStatement().executeUpdate("UPDATE `order` SET `status` = '" + status + "' WHERE `itemID` = " + orderID);
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	public static void deleteOrder(int orderIDInput)
+	{
+		try
+		{
+			// all database values will change once database is setup
+			Connection connection = LoginDataAccess.verifyCredentials();
+			String queryDeleteTransaction = "DELETE FROM order WHERE `orderID`.orderIDInput = "
+					+ orderIDInput;
+			PreparedStatement delete = connection.prepareStatement(queryDeleteTransaction);
+			delete.executeUpdate();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 	public Date getDate() {
 		return date;
