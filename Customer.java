ad51ebd2490f8d7971c3b116d62afcb6138b78c7
dante <79384307+DanteMar@users.noreply.github.com>
2022-05-07 22:31:58 -0700

    fixed queries

--- Customer.java
+++ Customer.java
@@ -1,15 +1,16 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Connection;
+import java.sql.Date;
 
 public class Customer {
-	private int customerID;
 	private String first_name;
 	private String last_name;
-	private int dob;
+	private Date dob;
 	private int phone;
 	private String street;
-	private String address_number;
+	private int address_number;
 	private int zip_code;
 	private String city;
 	private String state;
@@ -19,25 +20,32 @@ public class Customer {
 	{
 	}
 	
-	public void newCustomer(String first_name, String last_name, int dob, int phone, String street, String address_number, int zip_code, String city, String state)
+	public void newCustomer(String first_name, String last_name, Date dob, int phone, String street, int address_number, int zip_code, String city, String state)
 	{
 		try
 		{
 			Connection connection = LoginDataAccess.verifyCredentials();
-			// create the java statement
+			PreparedStatement stmt = connection.prepareStatement("INSERT INTO customer (first_name, last_name, dob, phone, street, address_number, zip_code, city, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+			stmt.setString(1, first_name);
+			stmt.setString(2, last_name);
+			stmt.setDate(3, dob);
+			stmt.setInt(4, phone);
+			stmt.setString(5, street);
+			stmt.setInt(6, address_number);
+			stmt.setInt(7, zip_code);
+			stmt.setString(8, city);
+			stmt.setString(9, state);
 			
-			//Generate appropriate query
-			String query = "INSERT INTO `user` (`first_name`, `last_name`, `dob`, `phone`, `street`, `address_number`, `zip_code`, `city`, `state`)";
+			stmt.executeUpdate();
 			
-			// execute the query
-			connection.createStatement().executeUpdate(query);
 		}
 		catch (Exception ex)
 		{
 			System.out.println(ex);
 		}
 	}
-	public void updateCustomer(int userID, String first_name, String last_name, int dob, int phone, String street, String address_number, int zip_code, String city, String state)
+	public void updateCustomer(int customerID, String first_name, String last_name, Date dob, int phone, String street, int address_number, int zip_code, String city, String state)
 	{
 		try
 		{
@@ -46,19 +54,38 @@ public class Customer {
 			
 			//Generate appropriate query
 			// all database values will change once database is setup
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `first_name` = '" + first_name + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `last_name` = '" + last_name + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `dob` = '" + dob + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `phone` = '" + phone + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `street` = '" + street + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `address_number` = '" + address_number + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `zip_code` = '" + zip_code + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `city` = '" + city + "' WHERE `userID` = " + userID);	
-			connection.createStatement().executeUpdate("UPDATE `customer` SET `state` = '" + state + "' WHERE `userID` = " + userID);	
+			
+			PreparedStatement stmt = connection.prepareStatement("update customer set first_name = ?, last_name = ?, dob = ?, phone = ?, street = ?, address_number = ?, zip_code = ?, city = ?, state = ? where customerid = ?");
+			stmt.setString(1, first_name);
+			stmt.setString(2, last_name);
+			stmt.setDate(3, dob);
+			stmt.setInt(4, phone);
+			stmt.setString(5, street);
+			stmt.setInt(6, address_number);
+			stmt.setInt(7, zip_code);
+			stmt.setString(8, city);
+			stmt.setString(9, state);
+			stmt.setInt(10, customerID);
+			
+			stmt.executeUpdate();
 		}
 		catch (Exception ex)
 		{
 			System.out.println(ex);
+		}
+	}
+	public void deleteCustomer(int customerIDInput)
+	{
+		try
+		{
+			// all database values will change once database is setup
+			Connection connection = LoginDataAccess.verifyCredentials();
+			PreparedStatement stmt = connection.prepareStatement("DELETE FROM customer WHERE customerid = ?");
+			stmt.setInt(1, customerIDInput);
+			stmt.executeUpdate();
+		}
+		catch (Exception e) {
+			System.out.println(e);
 		}
 	}
 }
