193db3ee4d4611af1846fe9e76791f4aed30b412
dante <79384307+DanteMar@users.noreply.github.com>
2022-05-07 23:13:47 -0700

    added create query and changed variables

--- Food_Order.java
+++ Food_Order.java
@@ -1,16 +1,40 @@
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+
 public class Food_Order {
-	private Food food;
+	private int menuitemID;
+	private int orderID;
 	private int quantity;
 	
-	public Food_Order(Food f,int q) {
-		food=f;
-		quantity=q;
+	public Food_Order() 
+	{
 	}
-	public Food getFood() {
-		return food;
+	
+	public void newFoodOrder(int menuitemID, int orderID, int quantity)
+	{
+		try
+		{
+			Connection connection = LoginDataAccess.verifyCredentials();
+			PreparedStatement stmt = connection.prepareStatement("INSERT INTO foodorder (menuitemID, orderID, quantity) VALUES (?, ?, ?)");
+
+			stmt.setInt(1, menuitemID);
+			stmt.setInt(2, orderID);
+			stmt.setInt(3, quantity);
+			
+			stmt.executeUpdate();
+			
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
 	}
-	public void setFood(Date f) {
-		food=f;
+	public int getmenuitemID() {
+		return menuitemID;
+	}
+	public int getorderID() {
+		return orderID;
 	}
 	public int getQuantity() {
 		return quantity;
