c505bcd249077acc980e43afb2fa40250c677437
dante <79384307+DanteMar@users.noreply.github.com>
2022-05-08 12:45:21 -0700

    updated queries

--- HistoricalPrice.java
+++ HistoricalPrice.java
@@ -6,17 +6,37 @@ public class HistoricalPrice {
 	private int priceID;
 	private double price;
 	private Date d;
+	private int menuitemID;
 	
-	public void newHistoricalPrice(double price, Date d)
+	HistoricalPrice()
+	{
+		
+	}
+	public void newHistoricalPrice(double price, Date d, int menuitemid)
 	{
 		try
 		{
 			Connection connection = LoginDataAccess.verifyCredentials();
-			PreparedStatement stmt = connection.prepareStatement("INSERT INTO historicalprice (price, date) VALUES (?, ?)");
+			PreparedStatement stmt = connection.prepareStatement("INSERT INTO historicalprice (hprice, hdate, menuitemid) VALUES (?, ?, ?)");
 
 			stmt.setDouble(1, price);
 			stmt.setDate(2, d);
+			stmt.setInt(3, menuitemid);
 			
+			stmt.executeUpdate();
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	public void deleteHistoricalPrice(int menuitemid)
+	{
+		try
+		{
+			Connection connection = LoginDataAccess.verifyCredentials();
+			PreparedStatement stmt = connection.prepareStatement("DELETE FROM historicalprice WHERE menuitemid = ?");
+			stmt.setInt(1, menuitemid);
 			stmt.executeUpdate();
 		}
 		catch (Exception ex)
