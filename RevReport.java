7b2547e868e5f0e6df52cce6375916cfea87511b
dante <79384307+DanteMar@users.noreply.github.com>
2022-05-08 20:29:10 -0700

    fixed

--- RevReport.java
+++ RevReport.java
@@ -1,3 +1,4 @@
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,14 +15,13 @@ public class RevReport {
         	//Generate appropriate query
         	// all database values will change once database is setup
         	PreparedStatement stmt = connection.prepareStatement(
-        		"select sum(total) from aorder,customer where broncoid=bid AND aorder.date between ? and ?");
+        		"select sum(total) as sum_price from aorder where broncoid = ?  AND odate between ? and ?");
         	stmt.setInt(1, bid);
         	stmt.setDate(2, d1);
         	stmt.setDate(3, d2);
         	ResultSet rs = stmt.executeQuery();
-        	stmt.executeUpdate();
-        	stmt.close();
-        	return rs.getDouble("total");
+        	rs.next();
+            return rs.getDouble("sum_price");
         }
 	    catch (Exception ex)
 	    {
@@ -36,13 +36,12 @@ public class RevReport {
         {
         	Connection connection = LoginDataAccess.verifyCredentials();
         	PreparedStatement stmt = connection.prepareStatement(
-        		"select sum(total) from aorder where aorder.date between ? and ?");
+        	"select sum(total) as sum_price from aorder where odate between ? and ?");
         	stmt.setDate(1, d1);
         	stmt.setDate(2, d2);
         	ResultSet rs = stmt.executeQuery();
-        	stmt.executeUpdate();
-        	stmt.close();
-            return rs.getDouble("total");
+        	rs.next();
+            return rs.getDouble("sum_price");
         }
         catch (Exception ex)
         {
