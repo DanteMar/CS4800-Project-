6889bfd01f264b975622817e0834970ec43a0dde
Vincent Lee <vinnyclee@gmail.com>
2022-05-10 22:33:18 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -76,6 +76,12 @@ public class Customer {
 			stmt.setInt(5, broncoID);
 			
 			stmt.executeUpdate();
+			
+			this.broncoID = broncoID;
+			this.first_name = first_name;
+			this.last_name = last_name;
+			this.dob = dob;
+			this.phone = phone;
 		}
 		catch (Exception ex)
 		{
